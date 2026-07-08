0355e25472166705992b9bbfd9022f46f0849b5c
Corno Schraverus <corno@schraverus.com>
2026-07-08 13:40:16 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/concrete_syntax_tree_from_untyped_syntax_tree_refiner/prose.ts
+++ typescript/lib/src/implementation/manual/transformers/concrete_syntax_tree_from_untyped_syntax_tree_refiner/prose.ts
@@ -1,17 +1,6 @@
 import * as p_ from 'pareto-core/implementation/transformer'
-import type * as p_i from 'pareto-core/interface/transformer'
 
-//data types
-import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
-import type * as d_in from "../../../../interface/data/concrete_syntax_tree_from_ast.js"
-
-export namespace interface_ {
-    export type Error = p_i.Transformer<
-        d_in.Error,
-        d_out.Phrase
-    >
-}
-import * as temp_interface_ from "../../../../interface/declarations/transformers/concrete_syntax_tree_from_untyped_syntax_tree_refiner/prose.js"
+import type * as interface_ from "../../../../interface/declarations/transformers/concrete_syntax_tree_from_untyped_syntax_tree_refiner/prose.js"
 
 //dependencies
 import * as t_path_to_text from "pareto-resources/implementation/manual/transformers/unrestricted_path/text"
