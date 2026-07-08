280b5b1a9590d950f390e9a0a9e8d0446afdf042
Corno Schraverus <corno@schraverus.com>
2026-07-08 12:47:35 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/concrete_syntax_tree_from_untyped_syntax_tree_refiner/prose.ts
+++ typescript/lib/src/interface/declarations/transformers/concrete_syntax_tree_from_untyped_syntax_tree_refiner/prose.ts
@@ -1,12 +1,12 @@
 
-import type * as p_i from 'pareto-core/interface/transformer'
+import type * as p_ from 'pareto-core/interface/transformer'
 
 //data types
 import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/prose/data"
 import type * as d_in from "../../../data/concrete_syntax_tree_from_ast.js"
 
 
-    export type Error = p_i.Transformer<
+    export type Error = p_.Transformer<
         d_in.Error,
         d_out.Phrase
     >
